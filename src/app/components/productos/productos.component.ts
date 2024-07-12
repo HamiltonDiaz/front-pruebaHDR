8730341949b9f1f48d99a819d2e18e13e43c0309
HamiltonDiaz <54079326+HamiltonDiaz@users.noreply.github.com>
2024-07-12 02:52:09 -0500

    se finaliza consumo productos

--- src/app/components/productos/productos.component.ts
+++ src/app/components/productos/productos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-productos',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './productos.component.css'
 })
 export class ProductosComponent {
+  producto={
+    nombre:'',
+    descripcion:'',
+    categoriaProducto:''
+  }
 
+  constructor(private productosService: ProductosService){}
+  onSubmit() {
+    this.productosService.submitProducto(this.producto).subscribe(
+      response => {
+        alert('Producto creado correctamente');
+      },
+      error => {
+        console.error(error);
+        alert('Error al crear producto');
+      }
+    );
+  }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
+  private apiUrl = `${environment.apiUrl}/productos`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  submitProducto(producto: any): Observable<any> {
+    return this.http.post(this.apiUrl, producto);
+  }
 }
